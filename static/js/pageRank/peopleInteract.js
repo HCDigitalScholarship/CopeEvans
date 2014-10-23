964b1975eee40c7c54de76c4cbfa23963af10ede
Brandon Smith <bsmith8108@gmail.com>
2014-10-23 13:39:48 -0400

    fixed the author select in the letter map

--- static/js/pageRank/peopleInteract.js
+++ static/js/pageRank/peopleInteract.js
@@ -10,24 +10,16 @@ $(document).ready(function() {
 	var people = $("#people").children();
 	var names = [];
 	for (var i=0; i<people.length; i++) {
-	    names.push(people[i].innerHTML);
+	    var temp = people[i].href;
+	    temp = temp.split("z/")[1];
+	    temp = temp.split("/")[0];
+	    console.log("temp:",temp);
+	    temp = parseInt(temp,10);
+	    names.push(temp);
 	}
-	name_ids = []
-	for (var j=0; j<names.length; j++) {
-	    var found = false;
-	    var name = names[j].split(":")[0];
-	    var name = name.split(/(?=[A-Z])/).join(" ");
-	    console.log("name: ", name);
-	    for(var k=0; k<parent.people_data.length; k++) {
-		if (!found && name == parent.people_data[k].name) {
-		    name_ids.push(parent.people_data[k].id);
-		    found = true;
-		}
-	    }   
-	}
-	console.log("names: ", names);
-	console.log("filterDict: ", parent.filterDict);
-	parent.filterDict["author"] = name_ids;
+
+	parent.filterDict["author"] = (names);
+	console.log("filterDict: ", parent.filterDict["author"]);
 	parent.filterMap();
 	parent.updateNameBox();
 	parent.closeIframe();
